95c8be874cbdcb19a0fe314bb729652556bcf0da
Tom Anderson <tom.h.anderson@gmail.com>
2015-01-29 11:03:10 -0700

    deploy

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,4 +1,1 @@
 run "ln -nfs #{config.release_path}/config/autoload/local.php.dist #{config.release_path}/config/autoload/local.php"
-run "rm #{config.release_path}/data/doctrine.db"
-run "php #{config.release_path}/public/index.php orm:schema-tool:create"
-run "php #{config.release_path}/public/index.php data-fixture:import"
